0a4aa2f157eb31418ef8bd5ea225639695e1e0d2
Ben.Stone <ben.stone@cgi.com>
2018-08-22 14:39:47 -0600

    jq testing

--- cf/tasks/blue-green-deploy.sh
+++ cf/tasks/blue-green-deploy.sh
@@ -43,7 +43,7 @@ echo "apps_url: shows the apps assigned to the hostname: ${apps_url}"
 
 # TO DO: Finish the jq parsing
 # Fetch the app name/s assigned to the hostname
-app_names=`(cf curl $apps_url | jq -r '...')`
+app_names=`cf curl $apps_url | jq -r '.resources[].entity | .name'`
 
 for name in $app_names; do
     if [ "$name" != "$app_name" ]
