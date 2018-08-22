17aa775cf340c291c7f072e1c4db78999d1f98ab
Ben.Stone <ben.stone@cgi.com>
2018-08-22 14:54:56 -0600

    force delete bc automated

--- cf/tasks/blue-green-deploy.sh
+++ cf/tasks/blue-green-deploy.sh
@@ -48,7 +48,7 @@ app_names=`cf curl $apps_url | jq -r '.resources[].entity | .name'`
 for name in $app_names; do
     if [ "$name" != "$app_name" ]
     then
-      cf delete $name
+      cf delete $name -f
       # TO DO: clean up blue
     fi
 done
